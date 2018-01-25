f10e4f614fc9fc75b657fef505615f29c6c6102d
dann1 <dann1telecom@gmail.com>
2018-01-24 20:54:03 -0500

    rewritten shutdown input

--- src/remotes/vmm/lxd/shutdown.py
+++ src/remotes/vmm/lxd/shutdown.py
@@ -3,10 +3,13 @@
 import lxd_common as lc
 
 t0 = lc.time()
-client = lc.Client()
 
-VM_ID = lc.sys.argv[1]
-container = client.containers.get('one-' + VM_ID)
+DOMAIN = lc.sys.argv[1]
+# HOST = lc.sys.argv[2]
+VM_ID = lc.sys.argv[3]
+
+client = lc.Client()
+container = client.containers.get(DOMAIN)
 dicc = lc.xml_start(container.config['user.xml'])
 
 # CONTAINER_SHUTDOWN
